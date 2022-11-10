447df6c9a9ae622b87ec3c5d68c459a4af374b6b
FoxChalmers <114472006+FoxChalmers@users.noreply.github.com>
2022-11-10 15:06:51 +0100

    Update Volvo240.java

--- Volvo240.java
+++ Volvo240.java
@@ -4,6 +4,7 @@ public class Volvo240 extends Car{
 
     public final static double trimFactor = 1.25;
     
+    // Constructor
     public Volvo240(){
         setNrDoors(4);
         setColor(Color.black);
