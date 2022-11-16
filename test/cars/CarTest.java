d1a0730d928a69ea1a09eb35e9128d3c8c1127a1
FoxChalmers <114472006+FoxChalmers@users.noreply.github.com>
2022-11-16 14:15:07 +0100

    h

---
+++ test/cars/CarTest.java
@@ -0,0 +1,14 @@
+package Labb1;
+
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+
+/*public class ARSetTest {
+  @Test
+  public void empty_set_should_have_size_zero() {
+    ARSet emptySet = new ARSet();
+    assertEquals(0, emptySet.size());
+  }
+}*/
