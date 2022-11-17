c7001344bbeba106b40d9713b5382df9c641f10d
FoxChalmers <114472006+FoxChalmers@users.noreply.github.com>
2022-11-17 16:07:45 +0100

    Update Saab95Test.java

--- test/cars/Saab95Test.java
+++ test/cars/Saab95Test.java
@@ -4,9 +4,13 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 public class Saab95Test {
+  Saab95 testSaab = new Saab95();
+  @Test 
+  public void see_if_speedfactor_is_correctly_calculated() {
+    assertTrue(testSaab.speedFactor() == testSaab.getEnginePower()*0.01);
+  }
   @Test
-  public void runItBackTurbo() {
-    Saab95 testSaab = new Saab95();
+  public void see_if_turbo_method_change_speedfactor_according_to_plans() {
     assertTrue(testSaab.speedFactor() == testSaab.getEnginePower()*0.01);
     testSaab.setTurboOn();
     assertTrue(testSaab.speedFactor() == testSaab.getEnginePower()*0.01*1.3);
