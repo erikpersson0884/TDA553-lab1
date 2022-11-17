c49bd4ad0d4dd5df71ff4a726518a0a687733117
FoxChalmers <114472006+FoxChalmers@users.noreply.github.com>
2022-11-17 13:26:51 +0100

    rororor

---
+++ test/cars/Volvo240Test.java
@@ -0,0 +1,15 @@
+package cars;
+
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+
+public class Volvo240Test {
+  @Test
+  public void number_of_doors_should_be_2() {
+    Saab95 sabb95 = new Saab95();
+    assertEquals(2,2);
+
+  }
+  }
